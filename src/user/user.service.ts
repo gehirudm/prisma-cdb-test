554d04d8803ceb13745aad84bddeddf09f7368d3
gehirudm <gehirudm@pm.me>
2023-04-21 21:00:39 +0530

    commit to trigger build

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User, Prisma } from "@prisma/client";
 
+
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) { }
